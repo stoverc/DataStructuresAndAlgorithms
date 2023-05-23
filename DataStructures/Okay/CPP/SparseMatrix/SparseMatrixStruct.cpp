eab6074bab1700d912c082879152d236d3d14463
Chris Stover <chaz0014@gmail.com>
2023-05-23 14:57:24 -0500

    Added Add() functionality

--- DataStructures/Okay/CPP/SparseMatrix/SparseMatrixStruct.cpp
+++ DataStructures/Okay/CPP/SparseMatrix/SparseMatrixStruct.cpp
@@ -55,11 +55,71 @@ void Display(struct SparseMatrix s){
     }
 }
 
-int main(){
-    struct SparseMatrix s;
+struct SparseMatrix * Add(struct SparseMatrix *s1, struct SparseMatrix *s2){
+    struct SparseMatrix *sum;
 
-    Create(&s);
-    Display(s);
+    if(s1 -> m != s2 -> m || s1 -> n != s2 -> n){
+        std::cout << "Dimensions do not match!" << std::endl;
+        //return -1;
+    }
+
+    sum = new SparseMatrix;
+
+    sum -> m = s1 -> m;
+    sum -> n = s1 -> n;
+    sum -> ele = new Element[s1 -> numElts + s2 -> numElts]; //this is the max size; may not need all this space
+
+    int i = 0, j = 0, k = 0;
+
+    while(i < s1 -> numElts && j < s2 -> numElts){
+        if(s1 -> ele[i].i < s2 -> ele[j].i){
+            sum -> ele[k++] = s1 -> ele[i++];
+        }
+        else if (s1 -> ele[i].i > s2 -> ele[j].i){
+            sum -> ele[k++] = s2 -> ele[j++];
+        }
+        else{
+            if(s1 -> ele[i].j < s2 -> ele[j].j){
+                sum -> ele[k++] = s1 -> ele[i++];
+            }
+            else if(s1 -> ele[i].j > s2 -> ele[j].j){
+                sum -> ele[k++] = s2 -> ele[j++];
+            }
+            else{
+                sum -> ele[k] = s1 -> ele[i];
+                sum -> ele[k++].x = s1 -> ele[i++].x + s2 -> ele[j++].x;
+            }
+        }
+    }
+
+    for(; i < s1 -> numElts; i++){
+        sum -> ele[k++] = s1 -> ele[i];
+    }
+
+    for(; j < s2 -> numElts; j++){
+        sum -> ele[k++] = s2 -> ele[j];
+    }
+
+    sum -> numElts = k;
+
+    return sum;
+}
+
+int main(){
+    struct SparseMatrix s1, s2, *s3;
+
+    //Create(&s);
+    //Display(s);
+
+    Create(&s1);
+    Create(&s2);
+    s3 = Add(&s1,&s2);
+    std::cout << "First matrix:" << std::endl;
+    Display(s1);
+    std::cout << "Second matrix:" << std::endl;
+    Display(s2);
+    std::cout << "Their sum:" << std::endl;
+    Display(*s3);
 
     return 0;
 }
