9a3ce1f7d56777a560cf5d3b06a5581c6232c5e8
Chris Stover <chaz0014@gmail.com>
2023-06-07 13:20:47 -0500

    Added Length(), Sum(), Min(), Max()

--- DataStructures/Okay/CPP/LinkedList/LinkedList.cpp
+++ DataStructures/Okay/CPP/LinkedList/LinkedList.cpp
@@ -15,6 +15,10 @@ class LinkedList {
         Node<T>* GetHead();
 
         void Display();
+        int Length();
+        T Sum();
+        T Min();
+        T Max();
 };
 
 template <typename T>
@@ -66,11 +70,69 @@ void LinkedList<T>::Display(){
             std::cout << p -> GetData() << ",";
         }
         else{
-            std::cout << p -> GetData() << "]";
+            std::cout << p -> GetData() << "]" << std::endl;
         }
 
         p = p -> GetNext();
     }
+}
+
+template <typename T>
+int LinkedList<T>::Length(){
+    Node<T> *p = head;
+    int len = 0;
+
+    while(p){
+        len += 1;
+        p = p -> GetNext();
+    }
+
+    return len;
+}
+
+template <typename T>
+T LinkedList<T>::Sum(){
+    Node<T> *p = head;
+    int sum = 0;
+
+    while(p){
+        sum += p -> GetData();
+        p = p -> GetNext();
+    }
+
+    return sum;
+}
+
+template <typename T>
+T LinkedList<T>::Min(){
+    Node<T> *p = head;
+    T min = (T)INT_MAX;
+
+    while(p){
+        if(p -> GetData() < min){
+            min = p -> GetData();
+        }
+
+        p = p -> GetNext();
+    }
+
+    return min;
+}
+
+template <typename T>
+T LinkedList<T>::Max(){
+    Node<T> *p = head;
+    T max = (T)INT_MIN;
+
+    while(p){
+        if(p -> GetData() > max){
+            max = p -> GetData();
+        }
+
+        p = p -> GetNext();
+    }
+
+    return max;
 }
 
 int main(){
@@ -78,6 +140,10 @@ int main(){
     LinkedList<int> ll1(arr,5);
 
     ll1.Display();
+    std::cout << "The length is " << ll1.Length() << std::endl;
+    std::cout << "The sum is " << ll1.Sum() << std::endl;
+    std::cout << "The min is " << ll1.Min() << std::endl;
+    std::cout << "The max is " << ll1.Max() << std::endl;
 
     return 0;
 }
